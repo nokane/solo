ba3f8e2ec237b0a94b87b101355576058daee5b8
Niall O'Kane <okanenm@gmail.com>
2015-06-15 16:07:30 -0700

    Defining the interface.html and InterfaceController

---
+++ client/app/interface/interface.js
@@ -0,0 +1,6 @@
+angular.module('digit.interface', [])
+.controller('InterfaceController', function ($scope, $window, $location, $firebaseObject) {
+
+  var ref = new Firebase("https://dazzling-inferno-751.firebaseio.com");
+  
+});
