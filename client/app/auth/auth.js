58f9792f9b3bb6fe1f4120483dd6ccfa8b2a7a9b
Niall O'Kane <okanenm@gmail.com>
2015-06-15 16:10:30 -0700

    If GitHub auth is successful, redirect to /interface

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,6 +1,5 @@
 angular.module('digit.auth', [])
 .controller('AuthController', function ($scope, $window, $location, Auth, $firebaseAuth) {
-  $scope.user = {};
 
   var ref = new Firebase("https://dazzling-inferno-751.firebaseio.com");
   
@@ -9,13 +8,12 @@ angular.module('digit.auth', [])
   $scope.signin = function() {
     $scope.authObj.$authWithOAuthPopup("github").then(function(authData) {
       console.log("Logged in as:", authData.uid);
-      console.dir(authData);      
+      console.dir(authData);
+      $window.localStorage.setItem('digit', authData.token);
+      $location.path('/interface');
     }).catch(function(error) {
       console.error("Authentication failed:", error);
-
     });
   }
 
-
- 
 });
