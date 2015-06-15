0246fb287e03807864355ad3c10d5868f092c964
Niall O'Kane <okanenm@gmail.com>
2015-06-15 15:54:39 -0700

    Configuring Firebase Authentication via GitHub OAuth

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,25 +1,21 @@
 angular.module('digit.auth', [])
-.controller('AuthController', function ($scope, $window, $location, Auth) {
+.controller('AuthController', function ($scope, $window, $location, Auth, $firebaseAuth) {
   $scope.user = {};
-  $scope.signin = function () {
-    // Auth.signin($scope.user)
-    //   .then(function (token) {
-    //     $window.localStorage.setItem('com.shortly', token);
-    //     $location.path('/links');
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-  };
 
-  $scope.signup = function () {
-    // Auth.signup($scope.user)
-    //   .then(function (token) {
-    //     $window.localStorage.setItem('com.shortly', token);
-    //     $location.path('/links');
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-  };
+  var ref = new Firebase("https://dazzling-inferno-751.firebaseio.com");
+  
+  $scope.authObj = $firebaseAuth(ref);
+
+  $scope.signin = function() {
+    $scope.authObj.$authWithOAuthPopup("github").then(function(authData) {
+      console.log("Logged in as:", authData.uid);
+      console.dir(authData);      
+    }).catch(function(error) {
+      console.error("Authentication failed:", error);
+
+    });
+  }
+
+
+ 
 });
