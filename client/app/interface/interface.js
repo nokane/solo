f02d141343b6736c684052777a2b38b23c3e255b
Niall O'Kane <okanenm@gmail.com>
2015-06-16 01:40:56 -0700

    beginGame listens to /newgame/ on the Firebase DB if a new game has been created

--- client/app/interface/interface.js
+++ client/app/interface/interface.js
@@ -2,12 +2,24 @@ angular.module('digit.interface', [])
 .controller('InterfaceController', function ($scope, $window, $location, $firebaseObject, Auth) {
   // Auth.get
   $scope.challenger = {};
+  $scope.me = Auth.getMe();
+  $scope.actualChallenge = undefined;
   $scope.$on('newChallenge', function() {
     console.log("THE INTERFACE CONTROLLER HEARD THERE IS A NEW CHALLENGE");
     $scope.challenger = Auth.getChallenges()[0].challenger;
     console.log("YOUR CHALLENGER:", $scope.challenger);
     // Auth.deleteChallenges();
     $location.path('/interface/challenge');
-  })
+  });
+  var beginGame = new Firebase("https://dazzling-inferno-751.firebaseio.com/newgame/");
+  beginGame.on('child_added', function(childSnapshot, prevChildKey) {
+    console.log('THE GAME IS ABOUT TO BEGIN!!!!');
+    var deleteChallenge = new Firebase('https://dazzling-inferno-751.firebaseio.com/newgame');
+    deleteChallenge.remove();
+    if(Auth.comparePlayer(childSnapshot.val()[0])) {
+      $location.path('/game');
+    }
+
+  });
   
 });
