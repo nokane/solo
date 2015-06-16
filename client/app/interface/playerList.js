114a1545b472089b024ee75879c8d74cee803800
Niall O'Kane <okanenm@gmail.com>
2015-06-15 22:51:45 -0700

    configured ability to send challenges to other users

--- client/app/interface/playerList.js
+++ client/app/interface/playerList.js
@@ -1,9 +1,8 @@
 angular.module('digit.playerList', [])
-.controller('PlayerListController', function ($scope, $window, $location, $firebaseObject) {
+.controller('PlayerListController', function ($scope, $window, $location, $firebaseObject, Auth) {
 
-
-  //var loggedIn = new Firebase("https://dazzling-inferno-751.firebaseio.com/presence/");
   $scope.users = {};
+  $scope.me = Auth.getMe();
   $scope.displayLoggedIn = function() {
     var loggedIn = new Firebase("https://dazzling-inferno-751.firebaseio.com/presence/");
     var syncObject = $firebaseObject(loggedIn);
@@ -11,19 +10,18 @@ angular.module('digit.playerList', [])
     console.dir(syncObject);
     syncObject.$bindTo($scope,"users");
   };
-  $scope.challenge = function() {
+  $scope.challenge = function(userid, name) {
+    console.log("I CHALLENGE YOU")
+    console.log(userid);
+    var sendChallenge = new Firebase('https://dazzling-inferno-751.firebaseio.com/challenge/'+userid);
+    // var receiveChallenge = $firebaseObject(sendChallenge);
+    var newChallenge = sendChallenge.push({'recipient' : { 'userid': userid, 'username': name }, 'challenger' : $scope.me});
+    // var deleteChallenge = new Firebase=('https://dazzling-inferno-751.firebaseio.com/challenge/'+user '/' + newChallenge.key());
+    // deleteChallenge.remove();
 
+    // var challenge = new Firebase("https://dazzling-inferno-751.firebaseio.com/presence/");
   };
 
   $scope.displayLoggedIn();
 });
 
-
-// /app.controller("SampleCtrl", function($scope, $firebaseObject) {
-//   var ref = new Firebase("https://<YOUR-FIREBASE-APP>.firebaseio.com/data");
-//   // download the data into a local object
-//   var syncObject = $firebaseObject(ref);
-//   // synchronize the object with a three-way data binding
-//   // click on `index.html` above to see it used in the DOM!
-//   syncObject.$bindTo($scope, "data");
-// });/
