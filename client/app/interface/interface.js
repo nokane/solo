242ba0225ee4b811a04857825970dd998e66f5ff
Niall O'Kane <okanenm@gmail.com>
2015-06-15 22:50:11 -0700

    on newChallenge event, the location will be set to /interface/challenge

--- client/app/interface/interface.js
+++ client/app/interface/interface.js
@@ -1,6 +1,13 @@
 angular.module('digit.interface', [])
-.controller('InterfaceController', function ($scope, $window, $location, $firebaseObject) {
-
-  var ref = new Firebase("https://dazzling-inferno-751.firebaseio.com");
+.controller('InterfaceController', function ($scope, $window, $location, $firebaseObject, Auth) {
+  // Auth.get
+  $scope.challenger = {};
+  $scope.$on('newChallenge', function() {
+    console.log("THE INTERFACE CONTROLLER HEARD THERE IS A NEW CHALLENGE");
+    $scope.challenger = Auth.getChallenges()[0].challenger;
+    console.log("YOUR CHALLENGER:", $scope.challenger);
+    // Auth.deleteChallenges();
+    $location.path('/interface/challenge');
+  })
   
 });
