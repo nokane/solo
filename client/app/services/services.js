b89ebeb75a743f0631c788d36fa3e181d42de5d3
Niall O'Kane <okanenm@gmail.com>
2015-06-16 01:35:53 -0700

    Added comparePlayer()

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,9 +1,9 @@
 angular.module('digit.services', [])
 
-.factory('Auth', function ($http, $location, $window) {
+.factory('Auth', function ($http, $location, $window, $rootScope) {
   var challenges = [];
   var me = {};
-
+  var challenge = {}
   var addChallenge = function(obj) {
     challenges.push(obj);
   };
@@ -15,18 +15,39 @@ angular.module('digit.services', [])
     challenges = [];
   };
   var setMe = function(obj) {
+    console.log("ME HAS BE SET!!!!!!");
     me = obj;
+    console.dir(me);
   };
   var getMe = function() {
     return me;
+  };
+  var comparePlayer = function(obj) {
+    for(var key in obj) {
+      if (obj[key].userid === me.userid) {
+        console.log("YOU WILL BE IN A TYPING MATCH!");
+        // challenge = obj;
+        $location.path('/game');
+        $rootScope.$apply(function() {
+          console.log('apply the /game location');
+          $location.path('/game');
+        })
+        // return true;
+      }
+    }
+    return false;
   }
 
-  
   return {
+    comparePlayer: comparePlayer,
     setMe: setMe,
     getMe: getMe,
     addChallenge: addChallenge,
     getChallenges: getChallenges,
     deleteChallenges: deleteChallenges
-  };
+  }
+
+})
+.factory('Game', function ($location, $window) {
+  return {};
 });
