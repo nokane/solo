dd3478e61bb35482f991528e084a340d870683c2
Niall O'Kane <okanenm@gmail.com>
2015-06-15 19:52:07 -0700

    when a client is authenticated, they will be stored in the database as 'logged in'

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -9,8 +9,24 @@ angular.module('digit.auth', [])
     $scope.authObj.$authWithOAuthPopup("github").then(function(authData) {
       console.log("Logged in as:", authData.uid);
       console.dir(authData);
+
+      var user = new Firebase('https://dazzling-inferno-751.firebaseio.com/users/');
+      var insertUser = user.push();
+
+      insertUser.set(authData.github);
+      var amOnline = new Firebase('https://dazzling-inferno-751.firebaseio.com/.info/connected');
+      var userRef = new Firebase('https://dazzling-inferno-751.firebaseio.com/presence/' + authData.uid);
+      amOnline.on('value', function(snapshot) {
+        if (snapshot.val()) {
+          userRef.onDisconnect().remove();
+          authData.presence = true;
+          userRef.set(authData);
+        }
+      });
+
+
       $window.localStorage.setItem('digit', authData.token);
-      $location.path('/interface');
+      $location.path('/interface/playerList');
     }).catch(function(error) {
       console.error("Authentication failed:", error);
     });
