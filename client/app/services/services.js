f4b09414bfb9e17213207f103a3e8c8dd9943561
Niall O'Kane <okanenm@gmail.com>
2015-06-15 22:48:12 -0700

    storing UserID/Username and array of challenges in Auth factory

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,8 +1,32 @@
 angular.module('digit.services', [])
 
 .factory('Auth', function ($http, $location, $window) {
-  var signup = function() {
+  var challenges = [];
+  var me = {};
 
+  var addChallenge = function(obj) {
+    challenges.push(obj);
+  };
+  var getChallenges = function() {
+    return challenges;
+  };
+  var deleteChallenges = function() {
+    console.log("CHALLENGE WAS DELETED!!!");
+    challenges = [];
+  };
+  var setMe = function(obj) {
+    me = obj;
+  };
+  var getMe = function() {
+    return me;
   }
-  return {};
+
+  
+  return {
+    setMe: setMe,
+    getMe: getMe,
+    addChallenge: addChallenge,
+    getChallenges: getChallenges,
+    deleteChallenges: deleteChallenges
+  };
 });
